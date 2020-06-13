3c35d8f5e4387d2781fcb9bd1f3552f7a8d5b070
dytveerendra <63532874+dytveerendra@users.noreply.github.com>
2020-06-13 22:48:06 +0530

    pom.xml configured with testng

--- src/main/java/com/dyt/CICD_QATest/SmokeTest.java
+++ src/main/java/com/dyt/CICD_QATest/SmokeTest.java
@@ -1,11 +1,15 @@
 package com.dyt.CICD_QATest;
 
+import org.testng.annotations.Test;
+
 public class SmokeTest {
 	
+	@Test
 	public void TC01() {
 		System.out.println("TC01");
 	}
 	
+	@Test
 	public void TC02() {
 		System.out.println("TC02");
 	}
