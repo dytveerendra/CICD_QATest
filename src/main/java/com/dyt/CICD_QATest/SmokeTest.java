9fc90c0eca178f37487ee078c99d21c2f81caeff
dytveerendra <63532874+dytveerendra@users.noreply.github.com>
2020-06-13 22:38:14 +0530

    Commit2

--- src/main/java/com/dyt/CICD_QATest/SmokeTest.java
+++ src/main/java/com/dyt/CICD_QATest/SmokeTest.java
@@ -1,5 +1,13 @@
 package com.dyt.CICD_QATest;
 
 public class SmokeTest {
+	
+	public void TC01() {
+		System.out.println("TC01");
+	}
+	
+	public void TC02() {
+		System.out.println("TC02");
+	}
 
 }
